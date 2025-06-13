fc05bf4abc2412236932af1a245c6d94f4fb7c44
salonis <salonis@athmin.com>
2025-06-13 21:41:57 +0530

    Pass test for single number

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,4 +2,5 @@ export function add(input: string): number {
     if(input === "") return 0;
     return parseInt(input);
 }
+
   
--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -1,7 +1,14 @@
 import { add } from "../src/stringCalculator";
 
+// First test case
 describe("String Calculator", () => {
   it("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  // second test case
+  it("returns number itself for single number", () => {
+    expect(add("2")).toBe(2)
+  })
+
 });
